9b1be6813169496242ec34dc75b674a10c9e1e2c
Jakub Romaniec <jakub.romaniec@outlook.com>
2017-07-24 15:07:39 +0200

    Convert complex array to strings array

--- JavaScript/GetWindowVariables.js
+++ JavaScript/GetWindowVariables.js
@@ -1,14 +1,14 @@
-﻿windowCustomized = {};
+﻿customVariableNames = [];
 var iframe = document.createElement('iframe');
 iframe.onload = function () {
     var standardGlobals = Object.keys(iframe.contentWindow);
     for (var b in window) {
         var prop = window[b];
         if (window.hasOwnProperty(b) && prop && !prop.toString().includes('native code') && !standardGlobals.includes(b)) {
-            windowCustomized[b] = prop;
+            
+			customVariableNames.push(b);
         }
     }
-    //console.info(windowCustomized)
 };
 iframe.src = 'about:blank';
 document.body.appendChild(iframe);
