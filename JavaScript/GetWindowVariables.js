41e988957a20e383d6967150ce64b77c62c130c0
Jakub Romaniec <jakub.romaniec@outlook.com>
2017-07-24 13:59:42 +0200

    Add script for testing window object pollution

---
+++ JavaScript/GetWindowVariables.js
@@ -0,0 +1,16 @@
+﻿windowCustomized = {};
+var iframe = document.createElement('iframe');
+iframe.onload = function () {
+    var standardGlobals = Object.keys(iframe.contentWindow);
+    for (var b in window) {
+        var prop = window[b];
+        if (window.hasOwnProperty(b) && prop && !prop.toString().includes('native code') && !standardGlobals.includes(b)) {
+            windowCustomized[b] = prop;
+        }
+    }
+    //console.info(windowCustomized)
+};
+iframe.src = 'about:blank';
+document.body.appendChild(iframe);
+
+return windowCustomized;
